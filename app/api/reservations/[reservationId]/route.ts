63b251679e59c516c7518f35e5b8c1f9c77ff9ca
sultonoir <sultonalidrus5@gmail.com>
2023-06-16 16:19:21 +0700

    add status

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -67,7 +67,7 @@ export async function PUT(request: Request, { params }: { params: IParams }) {
         },
       },
     };
-    if (status !== "Completed") {
+    if (update.status !== "Completed") {
       update.user = {
         update: {
           notifi: {
